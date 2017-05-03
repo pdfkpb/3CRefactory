6c95fc1715b8c8231025a49fc8967d3f0587dbf8
pdfkpb <pdfkkpb@gmail.com>
2017-05-03 10:48:44 -0400

    GamefinishedState put together

--- src/GameFinishedState.java
+++ src/GameFinishedState.java
@@ -1,5 +1,50 @@
-/**
- * Created by Jethro Masangya on 5/1/2017.
- */
-public class GameFinishedState extends RunState {
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Vector;
+
+public class GameFinishedState extends RunState
+{
+    @Override
+    public void run(Party party, boolean partyAssigned, boolean gameFinished, boolean gameIsHalted, Iterator bowlerIterator, Bowler currentThrower, boolean canThrowAgain, boolean tenthFrameStrike, int ball, Pinsetter setter, int frameNumber, int[][] finalScores, int bowlIndex, int gameNumber, int[][] cumulativeScores, HashMap scores, LaneEvent laneEvent, Vector subscribers)
+    {
+        EndGamePrompt endGamePrompt = new EndGamePrompt( ((Bowler) party.getMembers().get(0)).getNickName() + "'s Party" );
+        int result = endGamePrompt.getResult();
+        endGamePrompt.destroy();
+        endGamePrompt = null;
+
+
+        System.out.println("result was: " + result);
+
+        // TODO: send record of scores to control desk
+        if (result == 1) {					// yes, want to play again
+            resetScore(party, scores, gameFinished, frameNumber);
+            resetBowlerIterator(bowlerIterator, party);
+
+        } else if (result == 2) {           // no, dont want to play another game
+            Vector printVector;
+            EndGameReport endGameReport = new EndGameReport( ((Bowler)party.getMembers().get(0)).getNickName() + "'s Party", party);
+            printVector = endGameReport.getResult();
+            partyAssigned = false;
+            Iterator scoreIt = party.getMembers().iterator();
+            party = null;
+            partyAssigned = false;
+
+            publish(laneEvent, subscribers);
+
+            int myIndex = 0;
+            while (scoreIt.hasNext()){
+                Bowler thisBowler = (Bowler)scoreIt.next();
+                ScoreReport sr = new ScoreReport( thisBowler, finalScores[myIndex++], gameNumber );
+                //sr.sendEmail(thisBowler.getEmail());
+                Iterator printIt = printVector.iterator();
+                while (printIt.hasNext()){
+                    if (thisBowler.getNickName().equals(printIt.next())){
+                        System.out.println("Printing " + thisBowler.getNickName());
+                        sr.sendPrintout();
+                    }
+                }
+
+            }
+        }
+    }
 }
--- src/Lane.java
+++ src/Lane.java
@@ -233,45 +233,7 @@ public class Lane extends Thread implements PinsetterObserver {
 					}
 				}
 			} else if (partyAssigned && gameFinished) {
-				EndGamePrompt endGamePrompt = new EndGamePrompt( ((Bowler) party.getMembers().get(0)).getNickName() + "'s Party" );
-				int result = endGamePrompt.getResult();
-				endGamePrompt.destroy();
-				endGamePrompt = null;
-				
-				
-				System.out.println("result was: " + result);
-				
-				// TODO: send record of scores to control desk
-				if (result == 1) {					// yes, want to play again
-					resetScores();
-					resetBowlerIterator();
-					
-				} else if (result == 2) {           // no, dont want to play another game
-					Vector printVector;	
-					EndGameReport endGameReport = new EndGameReport( ((Bowler)party.getMembers().get(0)).getNickName() + "'s Party", party);
-					printVector = endGameReport.getResult();
-					partyAssigned = false;
-					Iterator scoreIt = party.getMembers().iterator();
-					party = null;
-					partyAssigned = false;
-					
-					publish(lanePublish());
-					
-					int myIndex = 0;
-					while (scoreIt.hasNext()){
-						Bowler thisBowler = (Bowler)scoreIt.next();
-						ScoreReport sr = new ScoreReport( thisBowler, finalScores[myIndex++], gameNumber );
-						//sr.sendEmail(thisBowler.getEmail());
-						Iterator printIt = printVector.iterator();
-						while (printIt.hasNext()){
-							if (thisBowler.getNickName().equals(printIt.next())){
-								System.out.println("Printing " + thisBowler.getNickName());
-								sr.sendPrintout();
-							}
-						}
 
-					}
-				}
 			}
 			
 			
